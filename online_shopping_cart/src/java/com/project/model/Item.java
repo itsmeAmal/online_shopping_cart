a70dabc65fdef858e5b056284b7d500fdac96947
Amal Wickramarathna <wickramarathna.amal@gmail.com>
2020-01-15 10:24:39 +0530

    add item Model class

---
+++ online_shopping_cart/src/java/com/project/model/Item.java
@@ -0,0 +1,257 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.project.model;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author Amal
+ */
+public class Item {
+
+    private int itemId;
+    private String itemName;
+    private String itemBrand;
+    private String itemModel;
+    private String itemSerial;
+    private String ramSize;
+    private String hardDiskSize;
+    private String ssdSize;
+    private String vgaTypeAndModel;
+    private String screenSize;
+    private String generation;
+    private int warrentyId;
+    private BigDecimal price;
+    private int itemStatus;
+    private String description1;
+    private String description2;
+
+    /**
+     * @return the itemId
+     */
+    public int getItemId() {
+        return itemId;
+    }
+
+    /**
+     * @param itemId the itemId to set
+     */
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    /**
+     * @return the itemName
+     */
+    public String getItemName() {
+        return itemName;
+    }
+
+    /**
+     * @param itemName the itemName to set
+     */
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    /**
+     * @return the itemBrand
+     */
+    public String getItemBrand() {
+        return itemBrand;
+    }
+
+    /**
+     * @param itemBrand the itemBrand to set
+     */
+    public void setItemBrand(String itemBrand) {
+        this.itemBrand = itemBrand;
+    }
+
+    /**
+     * @return the itemModel
+     */
+    public String getItemModel() {
+        return itemModel;
+    }
+
+    /**
+     * @param itemModel the itemModel to set
+     */
+    public void setItemModel(String itemModel) {
+        this.itemModel = itemModel;
+    }
+
+    /**
+     * @return the itemSerial
+     */
+    public String getItemSerial() {
+        return itemSerial;
+    }
+
+    /**
+     * @param itemSerial the itemSerial to set
+     */
+    public void setItemSerial(String itemSerial) {
+        this.itemSerial = itemSerial;
+    }
+
+    /**
+     * @return the ramSize
+     */
+    public String getRamSize() {
+        return ramSize;
+    }
+
+    /**
+     * @param ramSize the ramSize to set
+     */
+    public void setRamSize(String ramSize) {
+        this.ramSize = ramSize;
+    }
+
+    /**
+     * @return the hardDiskSize
+     */
+    public String getHardDiskSize() {
+        return hardDiskSize;
+    }
+
+    /**
+     * @param hardDiskSize the hardDiskSize to set
+     */
+    public void setHardDiskSize(String hardDiskSize) {
+        this.hardDiskSize = hardDiskSize;
+    }
+
+    /**
+     * @return the ssdSize
+     */
+    public String getSsdSize() {
+        return ssdSize;
+    }
+
+    /**
+     * @param ssdSize the ssdSize to set
+     */
+    public void setSsdSize(String ssdSize) {
+        this.ssdSize = ssdSize;
+    }
+
+    /**
+     * @return the vgaTypeAndModel
+     */
+    public String getVgaTypeAndModel() {
+        return vgaTypeAndModel;
+    }
+
+    /**
+     * @param vgaTypeAndModel the vgaTypeAndModel to set
+     */
+    public void setVgaTypeAndModel(String vgaTypeAndModel) {
+        this.vgaTypeAndModel = vgaTypeAndModel;
+    }
+
+    /**
+     * @return the screenSize
+     */
+    public String getScreenSize() {
+        return screenSize;
+    }
+
+    /**
+     * @param screenSize the screenSize to set
+     */
+    public void setScreenSize(String screenSize) {
+        this.screenSize = screenSize;
+    }
+
+    /**
+     * @return the generation
+     */
+    public String getGeneration() {
+        return generation;
+    }
+
+    /**
+     * @param generation the generation to set
+     */
+    public void setGeneration(String generation) {
+        this.generation = generation;
+    }
+
+    /**
+     * @return the warrentyId
+     */
+    public int getWarrentyId() {
+        return warrentyId;
+    }
+
+    /**
+     * @param warrentyId the warrentyId to set
+     */
+    public void setWarrentyId(int warrentyId) {
+        this.warrentyId = warrentyId;
+    }
+
+    /**
+     * @return the price
+     */
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    /**
+     * @return the itemStatus
+     */
+    public int getItemStatus() {
+        return itemStatus;
+    }
+
+    /**
+     * @param itemStatus the itemStatus to set
+     */
+    public void setItemStatus(int itemStatus) {
+        this.itemStatus = itemStatus;
+    }
+
+    /**
+     * @return the description1
+     */
+    public String getDescription1() {
+        return description1;
+    }
+
+    /**
+     * @param description1 the description1 to set
+     */
+    public void setDescription1(String description1) {
+        this.description1 = description1;
+    }
+
+    /**
+     * @return the description2
+     */
+    public String getDescription2() {
+        return description2;
+    }
+
+    /**
+     * @param description2 the description2 to set
+     */
+    public void setDescription2(String description2) {
+        this.description2 = description2;
+    }
+
+}
